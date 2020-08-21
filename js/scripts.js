03b2bdb4be8d9e68a57fbd188cb19cf241aa3d27
Jaki Widby <jaki.widby@gmail.com>
2020-08-21 09:14:38 -0700

    add functionality to 'createNumberArray' function, create click function to show results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-function CreateNumberArray(userInput) {
+function createNumberArray(userInput) {
   let numberArray = [];
-  for (let i = userInput; i >= 0; i --) {
-    
+  let i = 0;
+  for (let i = 0; i <= userInput; i ++) {
+    numberArray.push(" " + i);
   }
+  return numberArray;
+}
+
+$(document).ready(function() {
+  $("#generateList").click(function(event) {
+    event.preventDefault();
+    let userInput = parseInt($("input#userInput").val());
+    let numberArray = createNumberArray(userInput);
+    $("#showLater").text(numberArray);
+    console.log(numberArray);
+  })
+})
