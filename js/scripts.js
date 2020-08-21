6938c7d3c323bef1a05005611d30656f5f6c05af
Jaki Widby <jaki.widby@gmail.com>
2020-08-21 15:25:10 -0700

    add name parameter to 'createNumberArray' function, assign name value to variable, update alert message

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-function createNumberArray(userInput) {
+function createNumberArray(userInput, name) {
   let numberArray = [];
   let i = 0;
   if (!isNaN(userInput)) {
     for (let i = 0; i <= userInput; i ++) {
       let stringNumber = i.toString();
       if (stringNumber.includes("3")) {
-        numberArray.push(" Won't you be my neighbor?");
+        numberArray.push( " " + name + ", won't you be my neighbor?");
       } else if (stringNumber.includes("2")) {
         numberArray.push(" Boop!");
       } else if (stringNumber.includes("1")) {
@@ -16,13 +16,13 @@ function createNumberArray(userInput) {
     }
     return numberArray;
   } else {
-    alert("no");
+    alert("Kindly enter whole numbers only!");
   }
 }
 
 function checkForNegatives(userInput) {
   if (userInput < 0) {
-    alert("no");
+    alert("Kindly enter whole numbers only!");
   }
 }
 
@@ -30,8 +30,9 @@ $(document).ready(function() {
   $("#generateList").click(function(event) {
     event.preventDefault();
     let userInput = parseInt($("input#userInput").val());
+    let name = $("input#userName").val();
     checkForNegatives(userInput);
-    let numberArray = createNumberArray(userInput);
+    let numberArray = createNumberArray(userInput, name);
     $(".showLater").show();
     $("#results").text(numberArray);
   });
@@ -39,8 +40,9 @@ $(document).ready(function() {
   $("#reverseList").click(function(event) {
     event.preventDefault();
     let userInput = parseInt($("input#userInput").val());
+    let name = $("input#userName").val();
     checkForNegatives(userInput);
-    let numberArray = createNumberArray(userInput).reverse();
+    let numberArray = createNumberArray(userInput, name).reverse();
     $(".showLater").show();
     $("#results").text(numberArray);
 
